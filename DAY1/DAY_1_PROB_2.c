781c78053fb7131dc75637a305d656620216d380
Manu1229 <108973977+Manu1229@users.noreply.github.com>
2022-07-11 22:27:40 +0530

    Create DAY_1_PROB_2.c

---
+++ DAY1/DAY_1_PROB_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if ((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u'))
+    {
+     printf(" vowels ");
+    }
+    else
+    {
+    printf("consonant");
+    }
+}
